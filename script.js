04463497a94d567aab9ebbcf52017d7013424f8e
Tin Naing Oo <54192779+tinnaingoo@users.noreply.github.com>
2025-02-22 20:38:42 +0630

    Update script.js
    Signed-off-by: Tin Naing Oo <54192779+tinnaingoo@users.noreply.github.com>

--- script.js
+++ script.js
@@ -15,20 +15,23 @@ function toggleMenu() {
     navMenu.classList.toggle("active");
     hamburger.classList.toggle("active");
 }
-// Back to Top Button
-const backToTopBtn = document.getElementById("backToTopBtn");
 
-// Show the button when the user scrolls down 200px
-window.onscroll = function() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        backToTopBtn.style.display = "block";
-    } else {
-        backToTopBtn.style.display = "none";
-    }
-};
+// Get the button:
+let mybutton = document.getElementById("myBtn");
 
-// Scroll to the top when the button is clicked
-backToTopBtn.addEventListener("click", function() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
-});
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
