b8d3b7eb97268f4124533cf63db8f22ee1f2e5fd
Tin Naing Oo <54192779+tinnaingoo@users.noreply.github.com>
2025-02-22 20:33:57 +0630

    Update script.js
    Signed-off-by: Tin Naing Oo <54192779+tinnaingoo@users.noreply.github.com>

--- script.js
+++ script.js
@@ -15,3 +15,20 @@ function toggleMenu() {
     navMenu.classList.toggle("active");
     hamburger.classList.toggle("active");
 }
+// Back to Top Button
+const backToTopBtn = document.getElementById("backToTopBtn");
+
+// Show the button when the user scrolls down 200px
+window.onscroll = function() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+};
+
+// Scroll to the top when the button is clicked
+backToTopBtn.addEventListener("click", function() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+});
