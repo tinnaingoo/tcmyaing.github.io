ab20b80b811b28b754a58c9b7a0151295e87a41a
Tin Naing Oo <54192779+tinnaingoo@users.noreply.github.com>
2025-02-26 16:20:29 +0630

    Update post-script.js
    Signed-off-by: Tin Naing Oo <54192779+tinnaingoo@users.noreply.github.com>

--- post/post-script.js
+++ post/post-script.js
@@ -20,3 +20,21 @@ span.onclick = function() {
 }
 
 
+// Toggle Search Container on Mobile
+const sbutton = document.getElementById("sbutton");
+const searchContainer = document.querySelector(".search-container");
+const logo = document.querySelector(".logo");
+const searchInput = document.getElementById("searchInput");
+
+sbutton.addEventListener("click", function () {
+    searchContainer.classList.toggle("active");
+    logo.classList.toggle("hide");
+    sbutton.classList.toggle("active");
+
+    // Clear search input when closing the search container
+    if (!searchContainer.classList.contains("active")) {
+        searchInput.value = ""; // Clear the input field
+        filterPosts(); // Optionally, reset the filtered posts
+    }
+    
+});
